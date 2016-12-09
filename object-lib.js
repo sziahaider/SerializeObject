64831b69be466261144c144fc216a612d424c527
sziahaider <sziahaider@gmail.com>
2016-12-09 13:19:24 -0500

    Create object-lib.js

---
+++ object-lib.js
@@ -0,0 +1,23 @@
+$.fn.serializeObject=function(){
+                var tempObj={};
+                $(this).find('[name]').each(function(){
+                    var name=$(this).attr('name');
+                    var value=$(this).val();
+                    
+                    var keyPath=name.split('->');
+                    createObject(tempObj, keyPath, value);
+                });
+
+                return tempObj;
+            }
+
+            function createObject(obj, keyPath, value) {
+                lastKeyIndex = keyPath.length-1;
+                for (var i = 0; i < lastKeyIndex; ++ i) {
+                    key = keyPath[i];
+                    if (!(key in obj))
+                    obj[key] = {}
+                    obj = obj[key];
+                }
+                obj[keyPath[lastKeyIndex]] = value;
+            }
